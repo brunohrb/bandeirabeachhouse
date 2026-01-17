12fbef33b038b51769760f6ab739726950488474
brunohrb <brunohrb@gmail.com>
2026-01-17 17:31:07 -0300

    Update smoobu.js

--- api/smoobu.js
+++ api/smoobu.js
@@ -1,31 +1,17 @@
 export default async function handler(req, res) {
   try {
-    const { start, end } = req.query;
+    const response = await fetch(
+      "https://api.smoobu.com/reservations",
+      {
+        headers: {
+          "Api-Key": process.env.SMOOBU_API_KEY,
+          "Accept": "application/json"
+        }
+      }
+    );
 
-    if (!start || !end) {
-      res.status(400).json({ error: "Datas inválidas" });
-      return;
-    }
-
- const response = await fetch(
-  `https://api.smoobu.com/reservations`,
-  {
-    method: "GET",
-    headers: {
-      "Api-Key": process.env.SMOOBU_API_KEY,
-      "Accept": "application/json"
-    }
-  }
-);
-
-
-    if (!response.ok) {
-      throw new Error("Erro ao acessar o Smoobu");
-    }
-
-const text = await response.text();
-res.status(200).send(text);
-
+    const text = await response.text();
+    res.status(200).send(text);
 
   } catch (err) {
     res.status(500).json({ error: err.message });
